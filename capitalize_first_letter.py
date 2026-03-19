1c280aa129c008493abcc58b043ddbc71f438e02
Duke Buerano <dukebueranoo@gmail.com>
2026-03-19 19:33:50 +0800

    dinagdag ko yung main_execution function para i-test yung custom capitalize logic natin, at kinabit ko na rin yung start button sa dulo

--- capitalize_first_letter.py
+++ capitalize_first_letter.py
@@ -31,3 +31,18 @@ def capitalize_first_letter(target_string):
             
     # Pagdikitin yung naka-capital na unang letra at yung naka-lowercase na natitira
     return capitalized_first + lowered_rest
+# Function para i-test at patakbuhin yung ginawa nating logic sa taas
+def main_execution():
+    # Mag-set ng test string na gulo-gulo ang casing
+    test_string = "hELLO wORLD"
+    
+    # Ipasa yung data dun sa custom function at i-save ang resulta
+    result_string = capitalize_first_letter(test_string)
+    
+    # I-print ang original at yung bagong string na naka-capitalize na
+    print("\033[96mOriginal:\033[0m '" + test_string + "'")
+    print("\033[92mResult:\033[0m   '" + result_string + "'")
+
+# Ito yung magsisilbing switch para umandar yung script
+if __name__ == "__main__":
+    main_execution()
